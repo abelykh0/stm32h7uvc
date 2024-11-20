d26cbf6f1c289c0844374d9b204c7bdf03421862
Andrey Belykh <abelykh@yahoo.ca>
2024-11-20 13:14:13 -0500

    Using BBGGRR palette instead of RRGGBB

--- Main/startup.cpp
+++ Main/startup.cpp
@@ -31,11 +31,11 @@ extern "C" void setup()
 	config.ImageQuality = 90;
 	HAL_JPEG_ConfigEncoding(&hjpeg, &config);
 
-	screen.SetAttribute(0x3F01);
+	screen.SetAttribute(0x3F10);
 	screen.Clear();
 
     // Frame
-	screen.SetAttribute(0x2A01);
+	screen.SetAttribute(0x2A10);
     screen.PrintAt(0, 0, "\xC9"); // ╔
     screen.PrintAt(TEXT_COLUMNS - 1, 0, "\xBB"); // ╗
     screen.PrintAt(0, TEXT_ROWS - 1, "\xC8"); // ╚
@@ -55,15 +55,15 @@ extern "C" void setup()
 	char buf[20];
     for (int i = 0; i < 64; i++)
     {
-    	screen.SetAttribute((i << 8) | 0x01);
+    	screen.SetAttribute((i << 8) | 0x10);
     	screen.PrintAt(3 + (i % 5) * 7, 3 + (i / 5) * 2, "\xDF\xDF\xDF\xDF\xDF\xDF"); // ▀▀▀▀▀▀
 
-    	screen.SetAttribute(0x2A01);
+    	screen.SetAttribute(0x2A10);
     	sprintf(buf, BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(i));
     	screen.PrintAt(3 + (i % 5) * 7, 2 + (i / 5) * 2, buf);
     }
 
-	screen.SetAttribute(0x3F01);
+	screen.SetAttribute(0x3F10);
     //screen.PrintAlignCenter(0, " Hello, world! ");
 }
 
